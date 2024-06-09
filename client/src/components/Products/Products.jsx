435320ae613bbd6fcd4acd1eeacd9daf4f069152
sachinchavda17 <sachinchavda321@gmail.com>
2024-06-09 18:31:11 +0530

    new backend created

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -5,7 +5,7 @@ const Products = ({ innerPage, headingText, products }) => {
     <div className="products-container">
       {!innerPage && <div className="sec-heading">{headingText}</div>}
       <div className="products">
-        {products?.data.map((item) => (
+        {products?.data?.map((item) => (
           <Product key={item.id} id={item.id} data={item.attributes} />
         ))}
       </div>
